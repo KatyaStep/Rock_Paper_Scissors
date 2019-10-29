491f4daab348da62a162b5fbeb17eee379548c41
Ekaterina Stepanova <katia.yurevna@gmail.com>
2019-10-29 13:04:43 -0700

    First commit
    - basic design
    - function  of player's choice
    - random function of ai choice

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ paper.addEventListener("click", function(){
 
 
 
-// Random choise for AI 
+// Random choice for AI 
 
 ai.addEventListener ("click", function(){
     var choice = Math.floor((Math.random() * 3) + 1);
@@ -98,7 +98,7 @@ ai.addEventListener ("click", function(){
     if ( choice === 1) {
         console.log("rock");
         choice = insertAiResult(rock);
-        
+
     }
     else if ( choice === 2){
         console.log("paper");
@@ -111,8 +111,7 @@ ai.addEventListener ("click", function(){
 })
 
 
-
-
+// insert results of player's choice
 function insertResult(choice){ 
     var newParagraph = document.createElement("P");
     newParagraph.setAttribute("class", "results_paragraph");
@@ -122,6 +121,7 @@ function insertResult(choice){
     return choice;
 }
 
+//insert results of ai's choice
 function insertAiResult(choice) {
     var newParagraph = document.createElement("P");
     newParagraph.setAttribute("class", "ai_results_paragraph");
